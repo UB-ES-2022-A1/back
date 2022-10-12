326485c14693f77477fbed1a11fadba423c139fa
Nazar <xxnazarxx@hotmail.es>
2022-10-12 23:03:55 +0200

    [UPD] Faltaba commit de la bd

--- populate_db.py
+++ populate_db.py
@@ -9,9 +9,10 @@ with app.app_context():
     User.query.delete()
     Service.query.delete()
 
-    for i in range(1,10):
+    for i in range(1,11):
         user = User(email="email"+str(i), pwd="password", name="name")
         user.save_to_db()
         for j in range (2,i//2):
             serviceT = Service(title="title_"+str(i)+"_"+str(j), user=user, description="description", price=i+j)
             serviceT.save_to_db()
+    db.session.commit()
