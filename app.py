e7f030327b1254cd7aff0727988a600e67742830
Ferran Espuña <fespunbe7@alumnes.ub.edu>
2022-10-19 17:42:33 +0200

    ahora si que va

--- app.py
+++ app.py
@@ -20,9 +20,6 @@ migrate = Migrate(app, db)
 db.app = app
 db.init_app(app)
 
-#todo esto funciona para testing, hay que cambiarlo cuando estemos en produccion
-with app.app_context():
-    populate(db)
 
 # registramos los blueprints de los recursos
 app.register_blueprint(users_bp)
@@ -37,4 +34,7 @@ def hello_world():  # put application's code here
 
 
 if __name__ == '__main__':
+    # todo esto funciona para testing, hay que cambiarlo cuando estemos en produccion
+    with app.app_context():
+        populate(db)
     app.run()
