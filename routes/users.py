8ebb29520a68c7f6d0bf17ddc87efda6686ca208
Nazar <xxnazarxx@hotmail.es>
2022-11-10 13:45:05 +0100

    [UPD] Endpoints

--- routes/users.py
+++ routes/users.py
@@ -43,12 +43,22 @@ class UserSchema(SQLAlchemyAutoSchema):
     @validates("email")
     def validates_email(self, value):
         """
-        This phone validates the email
+        This method validates the email
         :param value: the email
         :return: None. Raises an Exception
         """
         validator = validate.Email()
         return validator(value)
+
+    @validates("verified_email")
+    def verified(self, value):
+        """
+        This  method validates the verified_email
+        :param value: verified_email
+        :return: None. Raises an Exception
+        """
+        if value:
+            raise ValidationError("verified_email es un campo que no se puede editar")
 
     # TODO remove para crear el admin maximo. Se quita esta función se crea el admin y se vuelve a añadir la función. También se puede hacer atentando contra la base de datos.
     @validates("access")
@@ -63,7 +73,7 @@ user_schema_repr = UserSchema(only=("name", "email", "birthday"))
 # Para crear usuario
 user_schema_create = UserSchema()
 
-user_schema_profile = UserSchema(exclude=['pwd', 'access'])
+user_schema_profile = UserSchema(exclude=['pwd', 'access', 'wallet', 'verified_email'])
 user_schema_profile_adm = UserSchema(exclude=['pwd'])
 
 
@@ -74,8 +84,12 @@ def get_all_users():
     This method return all users
     :return: Response with a list of all users
     """
-    all_users = User.query.filter_by(confirm_email=True)
-    return jsonify(user_schema_repr.dump(all_users, many=True)), 200
+    if g.user.access == 9 or g.user.access == 8:
+        all_users = User.query.all()
+        return jsonify(user_schema_profile_adm.dump(all_users, many=True)), 200  # Mostramos los accesos de privilegio
+    else:
+        all_users = User.query.filter_by(verified_email=True)
+        return jsonify(user_schema_repr.dump(all_users, many=True)), 200
 
 
 @users_bp.route("/<string:email>", methods=["GET"])
@@ -88,11 +102,12 @@ def get_user(email):
     """
     usr = User.query.get(email)
     if not usr:
-        raise NotFound
+        raise NotFound("Usuario no encontrado")
     if g.user.access == 9 or g.user.access == 8:
         return jsonify(user_schema_profile_adm.dump(usr, many=False)), 200  # Mostramos los accesos de privilegio a los administradores
     if usr.verified_email:
-       return jsonify(user_schema_profile.dump(usr, many=False)), 200
+        return jsonify(user_schema_profile.dump(usr, many=False)), 200
+    raise NotFound("Usuario no encontrado")
 
 
 @users_bp.route("/<string:email>", methods=["DELETE"])
@@ -130,9 +145,10 @@ def create_user():
     new_user.pwd = User.hash_password(new_user.pwd)
     new_user.save_to_db()
 
-    send_email('REGISTER', new_user.generate_auth_token() , new_user.email)
+    send_email('REGISTER', new_user.generate_auth_token(), new_user.email)
 
-    return jsonify(user_schema_profile.dump(new_user, many=False)), 201
+    return "Verifica el mail", 201
+
 
 @users_bp.route("/<string:email>", methods=["PUT"])
 @auth.login_required(role=[access[1], access[8], access[9]])
@@ -158,6 +174,7 @@ def edit_user(email):
     db.session.commit()
     return Response("Se ha editado correctamente el usuario con identificador: " + str(email), status=200)
 
+
 @users_bp.route("/<string:email>/privileges/<int:privilege>", methods=["PUT"])
 @auth.login_required(role=[access[9]])
 def changes_privileges(email, privilege):
@@ -182,6 +199,11 @@ def changes_privileges(email, privilege):
 @users_bp.route("/confirm_email/<token>", methods=["GET"])
 @auth.login_required(role=[access[0], access[1], access[8], access[9]])
 def confirm_email(token):
+    """
+    This method is used to verify a user email.
+    :param token: Contains user information. Is sent to him in the email.
+    :return: Message.
+    """
     user: User = User.verify_auth_token(token)
     user.verified_email = True
     user.save_to_db()
