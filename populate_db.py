4796488489abeaa7bb405580453405528bc53c83
Fabian <fabianserranolpz@gmail.com>
2022-12-13 22:38:14 +0100

    Update populate_db.py

--- populate_db.py
+++ populate_db.py
@@ -10,47 +10,35 @@ def populate(db):
         user_email = "email" + str(i) + "@gmail.com"
 
         try:
-            user = User(email=user_email, pwd=User.hash_password("password"), name="Name" + str(i), verified_email=True)
+
+            user = User(email=user_email, pwd=User.hash_password("password"), name="Name" + str(i), verified_email=True, wallet=10000)
             user.save_to_db()
+
+            for j in range(2, i // 2):
+                serviceT = Service(title="title_" + str(i) + "_" + str(j), user=user, description="description",
+                                   price=i + j)
+                serviceT.save_to_db()
+
+            if i == 7:
+                serviceT = Service(title="I'm a programmer that makes programs",
+                                   user=user, description="i can program your computer. I like #cheEse!!!",
+                                   price=10)
+                serviceT.save_to_db()
+
+            if i == 8:
+                serviceT = Service(title="I'm a programmer that makes programs",
+                                   user=user, description="i can program your computer. I like computers.",
+                                   price=100)
+                serviceT.save_to_db()
+
+            if i == 9:
+                serviceT = Service(title="I'm a cheese maker that makes cheese",
+                                   user=user, description="i can also eat your cheese. I like cheese.",
+                                   price=50)
+                serviceT.save_to_db()
+
         except:
             db.session.rollback()
-            user = User.get_by_id(user_email)
-
-        for j in range(2, i // 2):
-
-            serviceT = Service(title="title_" + str(i) + "_" + str(j), user=user, description="description",
-                               price=i + j)
-            try:
-                serviceT.save_to_db()
-            except:
-                db.session.rollback()
-
-        if i == 7:
-            serviceT = Service(title="I'm a programmer that makes programs",
-                               user=user, description="i can program your computer. I like #cheEse!!!",
-                               price=10)
-            try:
-                serviceT.save_to_db()
-            except:
-                db.session.rollback()
-
-        if i == 8:
-            serviceT = Service(title="I'm a programmer that makes programs",
-                               user=user, description="i can program your computer. I like computers.",
-                               price=100)
-            try:
-                serviceT.save_to_db()
-            except:
-                db.session.rollback()
-
-        if i == 9:
-            serviceT = Service(title="I'm a cheese maker that makes cheese",
-                               user=user, description="i can also eat your cheese. I like cheese.",
-                               price=50)
-            try:
-                serviceT.save_to_db()
-            except:
-                db.session.rollback()
 
     user_a = User(email="madmin@gmail.com", pwd=User.hash_password("password"), name="MaxAdm", access=9,
                   verified_email=True)
